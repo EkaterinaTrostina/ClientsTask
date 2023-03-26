b84ad24ce807a7abce503246e449ad135b1af391
EkaterinaTrostina_ <e.trostina@wikium.ru>
2023-03-26 14:40:38 +0300

    Firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyA_bv6bSHG9ih9Vj77UUIU-zM1q3DjvW_o',
+    authDomain: 'clientstest-af936.firebaseapp.com',
+    projectId: 'clientstest-af936',
+    storageBucket: 'clientstest-af936.appspot.com',
+    messagingSenderId: '514120215682',
+    appId: '1:514120215682:web:6d20584e8b1a5100de189b',
+    measurementId: 'G-8TP24E0FKC',
+},
 };
